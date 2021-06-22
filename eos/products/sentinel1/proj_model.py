666d74361ebb0e0d836b9838f92413dbb590489f
Roland Akiki <r.akiki@kayrros.com>
2021-06-22 10:37:54 +0200

    Fixed Docstring

--- eos/products/sentinel1/proj_model.py
+++ eos/products/sentinel1/proj_model.py
@@ -46,7 +46,49 @@ class Sentinel1BaseModel(coordinates.CoordinateMixin, model.SensorModel):
                  apd_correction=True,
                  max_iterations=20,
                  tolerance=0.001):
-        
+        """Sentinel1BaseModel used to perform projection and localization\
+        in a Sentinel1 image. 
+
+        Parameters
+        ----------
+        first_row_time: float
+            Azimuth time of the first line in the image
+        first_col_time: float
+            Two way slant range time of the first column in the image
+        approx_geom: list of tuples (lon, lat)
+            Approximate geometry of the image (represented by 4 corners)
+        range_frequency : float
+            Two way range time sampling frequency .
+        azimuth_frequency : float
+            Azimuth time sampling frequency.
+        slant_range_time : float
+            Two way time to the first column in the sentinel1 raster.
+        samples_per_burst : int
+            Number of columns per burst in the sentinel1 raster.
+        state_vectors : Iterable of dict
+            List of state vectors (time, position, velocity).
+        degree : int, optional
+            Degree of the orbit polynomial. The default is 11.
+        bistatic_correction : Boolean, optional
+            Apply bistatic correction on the azimuth time. The default is True.
+        apd_correction : Boolean, optional
+            Apply atmospheric correction on the range. The default is True.
+        max_iterations : int, optional
+            Maximum iterations of the iterative projection and localization
+            algorithms. The default is 20.
+        tolerance : float, optional
+            Tolerance on the geocentric position used as a stopping criterion.
+            For localization, tolerance is taken on 3D point position,
+            iterations stop when the step in x, y, z is less than tolerance.
+            For projection, the tolerance is considered on the satellite
+            position of closest approach. Converted to azimuth time tolerance
+            using the speed. The default is 0.001.
+
+        Returns
+        -------
+        None.
+
+        """
         self.range_frequency = range_frequency
         self.azimuth_frequency = azimuth_frequency
         self.slant_range_time = slant_range_time
@@ -228,7 +270,7 @@ class Sentinel1BaseModel(coordinates.CoordinateMixin, model.SensorModel):
         return x, y, z
 
 class Sentinel1BurstModel(Sentinel1BaseModel):
-    """Enables operations like projection and localization."""
+    """Enables operations like projection and localization at the burst."""
 
     def __init__(self,
                  range_frequency,
@@ -317,7 +359,7 @@ class Sentinel1BurstModel(Sentinel1BaseModel):
         self.azt_init = (self.burst_times[1] + self.burst_times[2])/2
 
 class Sentinel1SwathModel(Sentinel1BaseModel):
-    """Enables operations like projection and localization."""
+    """Enables operations like projection and localization at a swath."""
 
     def __init__(self,
                  range_frequency,
@@ -333,7 +375,52 @@ class Sentinel1SwathModel(Sentinel1BaseModel):
                  apd_correction=True,
                  max_iterations=20,
                  tolerance=0.001):
-        
+        """Sentinel1SwathModel used to perform projection and localization\
+        in a Sentinel1 swath.
+
+        Parameters
+        ----------
+        range_frequency : float
+            Two way range time sampling frequency .
+        azimuth_frequency : float
+            Azimuth time sampling frequency.
+        slant_range_time : float
+            Two way time to the first column in the sentinel1 raster.
+        samples_per_burst : int
+            Number of columns per burst in the sentinel1 raster.
+        bursts_times : list of (3,) tuple (start_time, start_valid, end_valid)
+            start_time is the azimuth time of the first line in the burst
+            start/end_valid denote the azimuth time of the
+            first/last valid line in the burst.
+        bursts_rois : list of (4,) tuple (x, y, w, h)
+            Coordinates of the burst in the sentinel-1 raster file.
+        bursts_approx_geom : List of list of tuples
+            Each element is a list of tuples (lon, lat) corners of the approx geom
+            of the burst
+        state_vectors : Iterable of dict
+            List of state vectors (time, position, velocity).
+        degree : int, optional
+            Degree of the orbit polynomial. The default is 11.
+        bistatic_correction : Boolean, optional
+            Apply bistatic correction on the azimuth time. The default is True.
+        apd_correction : Boolean, optional
+            Apply atmospheric correction on the range. The default is True.
+        max_iterations : int, optional
+            Maximum iterations of the iterative projection and localization
+            algorithms. The default is 20.
+        tolerance : float, optional
+            Tolerance on the geocentric position used as a stopping criterion.
+            For localization, tolerance is taken on 3D point position,
+            iterations stop when the step in x, y, z is less than tolerance.
+            For projection, the tolerance is considered on the satellite
+            position of closest approach. Converted to azimuth time tolerance
+            using the speed. The default is 0.001.
+
+        Returns
+        -------
+        None.
+
+        """
         # set these for the CoordinateMixin
         first_row_time = bursts_times[0][1]  # start valid
        
