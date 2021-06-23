820b93a7db48499600d249d0fc9a5cdae0b5c288
Roland Akiki <r.akiki@kayrros.com>
2021-06-23 11:55:25 +0200

    modified reading func name

--- eos/products/sentinel1/io.py
+++ eos/products/sentinel1/io.py
@@ -4,24 +4,26 @@ import rasterio
 # read the burst arrays
 
 
-def read_burst(tiff_path, burst_roi):
-    """Read burst.
+def read_window(tiff_path, roi):
+    """Read window inside the tiff.
 
     Parameters
     ----------
     tiff_path : str
-        Path to the tiff of the burst subswath.
-    burst_roi : tuple
-        (x,y,w,h) location of the burst in the tiff file.
+        Path to the tiff of the subswath.
+    roi : tuple
+        (x,y,w,h) location to read from in the tiff file.
 
     Returns
     -------
-    burst_array : ndarray
-        burst np.complex64 image.
+    array : ndarray
+        np.complex64 image.
 
     """
-    x, y, w, h = burst_roi
+    x, y, w, h = roi
     with rasterio.open(tiff_path) as db:
-        burst_array = db.read(1, window=(
+        array = db.read(1, window=(
             (y, y+h), (x, x+w))).astype('complex64')
-    return burst_array
+    return array
+
+        
