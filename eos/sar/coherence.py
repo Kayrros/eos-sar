b375a1e7b1e5bb1fccd4a4cb6a5975f0d2c35b71
Anger Jérémy <angerj.dev@gmail.com>
2021-10-08 09:48:10 +0200

    coherence: fix set_borders_to_nan when filter_size=1

--- eos/sar/coherence.py
+++ eos/sar/coherence.py
@@ -30,10 +30,12 @@ def _modify_borders(coherence, filter_size):
         hs = list(map(lambda f: f // 2, filter_size))
     else:
         hs = (filter_size // 2, filter_size // 2)
-    coherence[:hs[0],:] = np.nan
-    coherence[-hs[0]:,:] = np.nan
-    coherence[:,:hs[1]] = np.nan
-    coherence[:,-hs[1]:] = np.nan
+    if hs[0]:
+        coherence[:hs[0],:] = np.nan
+        coherence[-hs[0]:,:] = np.nan
+    if hs[1]:
+        coherence[:,:hs[1]] = np.nan
+        coherence[:,-hs[1]:] = np.nan
 
 
 def on_pair(im1, im2, filter_size, eps=1e-10,
