c7b2738c6837b7a8bd58069fdd5e0a0b81790603
Anger Jérémy <angerj.dev@gmail.com>
2022-06-27 17:47:05 +0200

    coordinates: add to_col/row/azt/rng for GRDCoordinateMixin

--- eos/sar/coordinates.py
+++ eos/sar/coordinates.py
@@ -16,7 +16,7 @@ class SLCCoordinateMixin:
         azt = row / self.azimuth_frequency + self.first_row_time
         return azt
 
-    def to_rng(self, col):
+    def to_rng(self, col, azt=None):
         rng = (col / self.range_frequency + self.first_col_time) * \
             const.LIGHT_SPEED_M_PER_SEC / 2
         return rng
@@ -30,7 +30,7 @@ class SLCCoordinateMixin:
         row = (azt - self.first_row_time) * self.azimuth_frequency
         return row
 
-    def to_col(self, rng):
+    def to_col(self, rng, azt=None):
         col = (2 * rng / const.LIGHT_SPEED_M_PER_SEC - self.first_col_time) * \
             self.range_frequency
         return col
@@ -48,14 +48,30 @@ class GRDCoordinateMixin:
     range_pixel_spacing: float
     srgr: SRGRConverter
 
-    def to_azt_rng(self, row, col):
+    def to_azt(self, row):
         azt = row * self.azimuth_time_interval + self.first_row_time
+        return azt
+
+    def to_rng(self, col, azt):
         gr = col * self.range_pixel_spacing
         rng = self.srgr.gr_to_rng(gr, azt)
+        return rng
+
+    def to_azt_rng(self, row, col):
+        azt = self.to_azt(row)
+        rng = self.to_rng(col, azt)
         return azt, rng
 
-    def to_row_col(self, azt, rng):
+    def to_row(self, azt):
         row = (azt - self.first_row_time) / self.azimuth_time_interval
+        return row
+
+    def to_col(self, rng, azt):
         gr = self.srgr.rng_to_gr(rng, azt)
         col = gr / self.range_pixel_spacing
+        return col
+
+    def to_row_col(self, azt, rng):
+        row = self.to_row(azt)
+        col = self.to_col(rng, azt)
         return row, col
