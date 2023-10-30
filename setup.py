9c82e4f5d092deb6020781e2d2a24960aec01c9c
Anger Jérémy <angerj.dev@gmail.com>
2023-10-30 09:16:07 +0100

    setup: add teosar dependencies as optional for now

--- setup.py
+++ setup.py
@@ -30,7 +30,9 @@ setuptools.setup(
     include_package_data=True,
     entry_points='',
     extras_require={
-        'test': ['pytest', 's1m']
+        'test': ['pytest', 's1m'],
+        # dependencies that teosar requires
+        'teosar': ['tqdm', 'tifffile', 'tensorflow-cpu', 'tensorflow_probability'],
     },
     classifiers=[]
 )
