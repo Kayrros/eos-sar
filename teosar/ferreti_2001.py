951cea0a337eccbd6a3b7fa2717fc525cf62468a
Anger Jérémy <angerj.dev@gmail.com>
2023-11-06 16:58:21 +0100

    Revert "teosar/ferreti: integrate q and v in defo_linear"
    This reverts commit 2b3fc34ec250f710d2ce9991d042856cfafeaded.

--- teosar/ferreti_2001.py
+++ teosar/ferreti_2001.py
@@ -412,9 +412,8 @@ def final_periodogram(
     # now add back linear trend deformation
     defo_nonlinear = phi_residual / Cv  # (mm)
     defo_linear = (
-        Cv * times_differences_against_ref[:, np.newaxis] * v[np.newaxis, :]
-        + Cq * date_normal_baseline * q[np.newaxis, :]
-    )  # (radians)
+        times_differences_against_ref[:, np.newaxis] * v[np.newaxis, :]
+    )  # (mm)
     return q, v, gammas, col, row, defo_nonlinear, defo_linear
 
 
