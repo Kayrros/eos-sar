735cba7a48c3e3593246b8163a7c1f2537067963
Anger Jérémy <angerj.dev@gmail.com>
2023-11-20 09:53:43 +0100

    0.23.0

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ for m in ext_module:
 
 setuptools.setup(
     name="kayrros-eos-sar",
-    version="0.22.0",
+    version="0.23.0",
     description="",
     long_description=long_description,
     author="Kayrros",
