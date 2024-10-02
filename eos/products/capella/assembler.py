05ffd54fece0c87bcc5d6959c1dd69ad5447403b
arthurhauck <hauck.arthur@gmail.com>
2024-10-02 11:43:00 +0200

    capella.assembler.CapellaSLCProduct: add methods: 	- get_proj_model: to get the geometrical model 	- get_roi: to get a region of interest in image coordinates (row,col) from an input geometry given in (lon,lat) coordinates.

--- eos/products/capella/assembler.py
+++ eos/products/capella/assembler.py
@@ -55,3 +55,80 @@ class CapellaSLCProduct:
 
         # Instantiate an Orbit object
         self.orbit = self.metadata.get_orbit(orbit_degree=orbit_degree)
+        
+        
+        
+    def get_proj_model(self, max_iterations=20, tolerance=0.001, apd=False): 
+        """
+        Get a projection model.
+
+        Parameters
+        ----------
+        max_iterations: int, optional
+            Maximum iterations of the iterative projection and localization
+            algorithms. The default is 20.
+        tolerance: float, optional
+            Tolerance on the geocentric position used as a stopping criterion.
+            For localization, tolerance is taken on 3D point position,
+            iterations stop when the step in x, y, z is less than tolerance.
+            For projection, the tolerance is considered on the satellite
+            position of closest approach. Converted to azimuth time tolerance
+            using the speed. The default is 0.001.
+        apd : bool, optional
+            If True the atmospheric correction (ApdCorrection) is applied.
+
+        Returns
+        -------
+        proj_model: CapellaSLCBaseModel object
+            Projection model used to perform projection and localization in a Capella image.
+        """
+        
+        return self.metadata.get_proj_model(max_iterations=max_iterations, tolerance=tolerance, apd=apd)
+    
+    
+    
+    def get_roi(self, input_geometry, proj_model=None, dem_source=None, vert_crs=None):
+        """
+        Adapted from eos-sar.usage.tutorial.
+        
+        Find the Region Of Interest (ROI) that you want to study in the image.
+        To do so, we use the model to geolocate (project) the 3D points into the image.
+        
+        Parameters
+        ----------
+        input_geometry: list of 4 (lon,lat) tuples
+            List of the geographical coordinates (lon,lat) of the 4 corners of you region of intrest.
+        proj_model: CapellaSLCBaseModel object, optional
+            Projection model used to perform projection and localization in a Capella image. The default is None. 
+            In this case the self.get_proj_model() method is called.
+        dem_source: Digital Elevation Model (DEM), optional
+            Digital Elevation Model of the area covered by the image. The default is None. 
+            In this case the SRTM90 DEM is used.
+        vert_crs: str, optional
+            Vertical CRS of the DEM: defines the altitudes' reference surface. The default is None. 
+            In this case, it is assumed that the reference surface is already the WGS84 ellipsoid.
+
+        Returns
+        -------
+        roi_in_img: CapellaSLCBaseModel object
+            Projection model used to perform projection and localization in a Capella image.
+        """
+        
+        # Get the longitudes and latitudes of the corners of your ROI
+        lon = [pt[0] for pt in input_geometry]
+        lat = [pt[1] for pt in input_geometry]
+
+        # Get the altitudes of the corners of your ROI
+        if dem_source is None:
+            dem_source = eos.dem.get_any_source()
+        alt = dem_source.elevation(lon, lat)
+        
+        # Get the projection model to pass from 3D coordinates to image coordinates
+        if proj_model is None:
+            proj_model = self.get_proj_model()
+
+        # Peform the projection to get your ROI in image coordinates
+        rows, cols, incidences = proj_model.projection(lon, lat, alt, vert_crs=vert_crs)
+        roi_in_img = Roi.from_bounds_tuple(Roi.points_to_bbox(rows, cols))
+
+        return roi_in_img
