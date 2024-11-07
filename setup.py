3b7a80a9db02a1568f4d6b9f53d19f7cfc1a34bd
Roland Akiki <r.akiki@kayrros.com>
2024-11-07 12:25:49 +0100

    teosar: Simplify tf and add siphash

--- setup.py
+++ setup.py
@@ -35,9 +35,8 @@ setuptools.setup(
         "teosar": [
             "tqdm",
             "tifffile",
-            "tensorflow-probability",
-            "tensorflow",
-            "tf-keras~=2.0",
+            "tensorflow-probability[tf]",  # see https://github.com/tensorflow/probability/releases/tag/v0.24.0
+            "siphash24",  # pytools is required by pyopencl, and gives a warning: RecommendedHashNotFoundWarning: Unable to import recommended hash 'siphash24.siphash13',
             "pyopencl",
         ],
         # dependencies for usage at Kayrros
