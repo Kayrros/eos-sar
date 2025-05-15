99eceb36e51670b7308eead341f45e863cefd27f
Jérémy Anger <angerj.dev@gmail.com>
2025-05-15 13:36:21 +0200

    repo: move to src-layout packaging

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 from Cython.Build import cythonize
 
 ext_module = cythonize(
-    ("eos/products/sentinel1/_calibration.pyx", "eos/sar/simulator.pyx")
+    ("src/eos/products/sentinel1/_calibration.pyx", "src/eos/sar/simulator.pyx")
 )
 for m in ext_module:
     m.include_dirs = [np.get_include()]
