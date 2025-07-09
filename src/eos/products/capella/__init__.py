72609a1ae6d35018be8bd96a9dd6f9457f9c9457
rolandakiki <r.akiki@kayrros.com>
2025-07-09 16:35:49 +0200

    capella/slc_cropper: Add workflow for cropping/aligning a stack of SLC capella imgs

--- src/eos/products/capella/__init__.py
+++ src/eos/products/capella/__init__.py
@@ -3,3 +3,4 @@ from eos.products.capella import metadata as metadata
 from eos.products.capella import polynomial as polynomial
 from eos.products.capella import proj_model as proj_model
 from eos.products.capella import resampler as resampler
+from eos.products.capella import slc_cropper as slc_cropper
