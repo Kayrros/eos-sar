02077e143eb767044d32a553ebcd300da5b22051
rolandakiki <r.akiki@kayrros.com>
2026-04-13 18:33:09 +0200

    tests: Add nisar rtc test

--- tests/test_rtc.py
+++ tests/test_rtc.py
@@ -1,10 +1,19 @@
 import numpy as np
+from rasterio.warp import Resampling
 
 import eos.dem
+from eos.dem import DEMStitcherSource
 from eos.products import sentinel1
+from eos.products.nisar.metadata import (
+    Frequency,
+    NisarRSLCMetadata,
+)
+from eos.products.nisar.proj_model import NisarModel
+from eos.sar.io import RemoteH5Loader
 from eos.sar.orbit import Orbit
 from eos.sar.roi import Roi
 from eos.sar.rtc import RadiometricTerrainCorrector
+from eos.sar.simulator import SARSimulator
 
 
 def test_rtc_simulation(tmp_path):
@@ -21,3 +30,27 @@ def test_rtc_simulation(tmp_path):
     rtc = RadiometricTerrainCorrector(model, dem, roi)
     sim = rtc.get_simulation()
     np.save(tmp_path / "sim.npy", sim)
+
+
+def test_nisar_simulation(tmp_path):
+    rslc_path = "https://nisar.asf.earthdatacloud.nasa.gov/NISAR-SAMPLE-DATA/RSLC/NISAR_L1_PR_RSLC_001_030_A_019_2000_SHNA_A_20081012T060910_20081012T060926_D00402_N_F_J_001/NISAR_L1_PR_RSLC_001_030_A_019_2000_SHNA_A_20081012T060910_20081012T060926_D00402_N_F_J_001.h5"
+
+    frequency: Frequency = "A"
+
+    # replace with GeometryRoiProvider or other if you want
+    roi = Roi(0, 0, 500, 500)
+    dem_source = DEMStitcherSource()
+
+    loader = RemoteH5Loader(rslc_path)
+
+    with loader as h5py_file:
+        metadata = NisarRSLCMetadata.parse_metadata(h5py_file)
+        orbit = Orbit(sv=metadata.state_vectors, degree=11)
+        model = NisarModel.from_metadata(metadata, frequency, orbit)
+
+    # Then simulate
+    dem = model.fetch_dem(dem_source, roi)
+    simulator = SARSimulator(model, dem, dem_resampling=Resampling.cubic_spline)
+    simulation = simulator.simulate(roi)
+
+    np.save(tmp_path / "sim_nisar.npy", simulation)
